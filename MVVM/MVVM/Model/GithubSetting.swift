684ba937f50a904d345c74f21178c38c607e9839
GSThecar <thecar3774@gmail.com>
2021-09-06 23:19:12 +0900

    base networking with fetch githu  service

---
+++ MVVM/MVVM/Model/GithubSetting.swift
@@ -0,0 +1,14 @@
+//
+//  GithubSetting.swift
+//  MVVM
+//
+//  Created by 이덕화 on 2021/08/31.
+//
+
+import Foundation
+
+struct GithubSetting {
+    let language: String = "swift"
+    let userID: String = "all"
+    let sortType: String = "stars"
+}
